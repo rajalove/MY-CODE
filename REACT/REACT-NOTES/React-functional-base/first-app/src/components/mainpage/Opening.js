5353b8e68b6959d9fcd8075e584464474d84f38c
rajalove <rajasekharkoneti3@gmail.com>
2023-02-02 11:57:47 +0530

    success

---
+++ REACT/REACT-NOTES/React-functional-base/first-app/src/components/mainpage/Opening.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+let Opening=()=>
+{
+    return(
+        <React.Fragment>
+            <h1>hi raja</h1>
+        </React.Fragment>
+    );
+};
+
+export default Opening;
